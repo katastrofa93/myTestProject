b3202b570eb059e9088fec8630492f36b1f3aa9d
ilya <katastrofa.zvl@gmail.com>
2021-06-08 20:23:43 +0300

    Регистрация локальных файлов

---
+++ shop/scripts/index.js
@@ -0,0 +1,15 @@
+<meta charset="utf-8">
+<script type="text/javascript">
+	
+
+
+	document.querySelector(".burg-open").onclick = function () {
+	        document.querySelector(".overlay").classList.add(".overlay-open");
+        }
+
+    document.querySelector(".overlay-nav-btn").onclick = function () {
+	        document.querySelector(".menu-overlay").classList.remove(".overlay-open");
+        }
+
+
+</script>
